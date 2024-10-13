10d5a0b2e71d52253ef00115f9bdc58bbbd73e9f
Thimoclesse <39627184+Thimoclesse@users.noreply.github.com>
2024-10-13 20:19:18 +0200

    Ex 8: add guards for authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-// Import your components or views
 import HomePage from '../components/HomePage.vue';
 import ConversationsIndexPage from '../components/ConversationsIndexPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import store from '../store'; // Import the Vuex store
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: HomePage
+        component: HomePage,
     },
     {
         path: '/conversations',
         name: 'Conversations',
-        component: ConversationsIndexPage
-    }
+        component: ConversationsIndexPage,
+        beforeEnter: (to, from, next) => {
+            const isAuthenticated = !!store.getters.currentUser;
+            if (!isAuthenticated) {
+                next({ name: 'Home' });
+                console.log("Please authetificate before")
+
+            } else {
+                next();
+            }
+        },
+    },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
